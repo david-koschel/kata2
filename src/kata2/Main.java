8e180f7931bc7661e6330e417cad64acaffbd056
david-koschel <david-koschel@users.noreply.github.com>
2022-10-19 17:23:02 +0100

    version 3

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -8,13 +8,13 @@ public class Main {
     public static void main(String[] args) {
 
         int[] data = new int[]{1, 2, 3, 4, 6, 1, 3, 65, 3, 2, 11, 5, 3, 1, 1};
-        Map<Integer, Integer> histogram = new HashMap<>();
+        
+        HistogramGenerator histogramGenerator = new HistogramGenerator(data);
 
-        for (int key : data) {
-            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
-        }
-        for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
-            System.out.println(entry.getKey() + "==>" + histogram.get(entry.getKey()));
+        Map<Integer,Integer> histogr = histogramGenerator.getHistogram();
+
+        for (Map.Entry<Integer, Integer> entry : histogr.entrySet()) {
+            System.out.println(entry.getKey() + "==>" + histogr.get(entry.getKey()));
         }
     }
 }
