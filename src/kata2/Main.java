9face6bf76ee54634699f73436098f24afa5c64a
david-koschel <david-koschel@users.noreply.github.com>
2022-10-19 17:18:24 +0100

    version 2

--- src/kata2/Main.java
+++ src/kata2/Main.java
@@ -10,12 +10,8 @@ public class Main {
         int[] data = new int[]{1, 2, 3, 4, 6, 1, 3, 65, 3, 2, 11, 5, 3, 1, 1};
         Map<Integer, Integer> histogram = new HashMap<>();
 
-        for (int i = 0; i < data.length; i++) {
-            if (histogram.containsKey(data[i])) {
-                histogram.put(data[i], histogram.get(data[i])+1);
-            } else {
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
         for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
             System.out.println(entry.getKey() + "==>" + histogram.get(entry.getKey()));
